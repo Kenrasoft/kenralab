a67c77b88f011f89367b0682659a3243d0d09a3a
Nash-kenra <nash.kenra@gmail.com>
2023-11-29 12:02:12 -0600

    added day2.js

---
+++ avinash/day2.js
@@ -0,0 +1,91 @@
+/* 
+1. Rewrite using Conditional operator:
+a.
+let x = 5;
+
+if (x > 0) {
+    console.log("Positive");
+} else if (x < 0) {
+    console.log("Negative");
+} else {
+    console.log("Zero");
+}
+    
+b.
+let age = 18;
+
+if (age >= 18) {
+    console.log("Eligible to vote");
+} else {
+    console.log("Not eligible to vote");
+}
+
+2. 
+a. Logical OR (||):
+    Question:
+    Write an if statement that checks if a variable value is either null or undefined.
+
+    Question:
+    Write a condition using || that assigns a default value of 10 to a variable count if it is falsy.
+
+    Question:
+    Check if a user has provided a valid email address. If not, use a default email address.
+
+b. Logical AND (&&):
+    Question:
+    Write an if statement that checks if a number is greater than 10 and less than 20.
+
+    Question:
+    Implement a condition using && that prints "Login successful" only if both the username and password are valid.
+
+    Question:
+    Check if a person is eligible to vote based on age (18 or older) and citizenship.
+
+c. Nullish Coalescing Operator (??):
+    Question:
+    Use the nullish coalescing operator to assign a default value of "Guest" to a variable username if it is null or undefined.
+
+    Question:
+    Implement a condition using ?? to set the value of result to 0 if value is null or undefined.
+
+    Question:
+    Check if a variable price is defined. If not, set it to the default value of 100 using the nullish coalescing operator.
+
+3. 
+a. Use for loop to generate these patterns
+b. use while loop to generate these patterns
+Pattern 1
+*****
+*****
+*****
+*****
+*****
+Pattern 2
+*****
+****
+***
+**
+*
+
+4. Write a JavaScript program that iterates through the numbers from 1 to 20 (inclusive). For multiples of 3, print "Fizz"; for multiples of 5, print "Buzz." However, if a number is a multiple of both 3 and 5, skip that iteration. (hint: 15 is skipped)
+Required Output:
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+16
+17
+Fizz
+19
+Buzz
+*/
