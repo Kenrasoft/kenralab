ae7b40fe93e750f7bb49dba11262a000cc216c81
Nash-kenra <nash.kenra@gmail.com>
2023-11-30 12:51:54 -0600

    day3 assignment

---
+++ avinash/day3.js
@@ -0,0 +1,11 @@
+/*
+1. Create an arrow function that returns the length of a string.
+2. Convert the following function to an arrow function.
+function square(x) {
+  return x * x;
+}
+3. Implement a function that reverses a string.
+4. Write a arrow and normal function that calculates the factorial of a number.
+5. Write arrow function, function expression, function that accepts a temperature in Celsius and converts it to Fahrenheit.
+6. Write differences between all four types of functions in javascript.
+*/
