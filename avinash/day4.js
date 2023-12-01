36b5aef4af84ced2b00474e05e73c5f95be4d2df
Nash-kenra <nash.kenra@gmail.com>
2023-12-01 12:01:33 -0600

    day4 assignment

---
+++ avinash/day4.js
@@ -0,0 +1,20 @@
+// Exercise 1:
+// Write a function calculator that takes two numbers and a callback function. The callback function should define a mathematical operation (e.g., add, subtract, multiply, divide), and the calculator function should return the result of applying the callback to the two numbers.
+
+// Exercise 2:
+// Create a function stringTransformer that takes a string and a callback function. The callback function should transform the string in the ways 1. capitalize, 2. reverse, and the stringTransformer function should return the transformed string.
+
+// Exercise 3:
+// Implement a function repeat that takes a callback function and a number n. The callback function should be executed n times.
+
+// Exercise 4:
+// Create a function counter that returns a closure. The closure should be a function that, when called, increments a counter and returns the current count.
+
+// Exercise 5:
+// Write a function powerOf that takes an exponent n and returns a function. The returned function should take a base x and calculate x raised to the power of n.
+
+// Exercise 6:
+// Create a function accumulator that returns a closure. The closure should be a function that, when called with a number, adds it to the accumulated sum and returns the updated sum.
+
+// Exercise 7:
+// Create a function customLogger that returns a closure. The closure should be a function that, when called with a message, logs the message along with a timestamp.
