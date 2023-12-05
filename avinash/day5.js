a4b95e3b53fb13f5c19abe75f55d253a9d3be40c
Nash-kenra <nash.kenra@gmail.com>
2023-12-05 15:37:55 -0600

    day5 assignment

---
+++ avinash/day5.js
@@ -0,0 +1,36 @@
+// Write the code, one line for each action:
+// a. Create an empty object user.
+// b. Add the property name with the value John.
+// c. Add the property surname with the value Smith.
+// d. Change the value of the name to Pete.
+// e. Remove the property name from the object.
+
+//2.
+// We have an object storing salaries of our team:
+
+// let salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130
+// }
+// Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
+// If salaries is empty, then the result must be 0.
+
+//3.
+// Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
+// For instance:
+// // before the call
+// let menu = {
+//   width: 200,
+//   height: 300,
+//   title: "My menu"
+// };
+// multiplyNumeric(menu);
+// // after the call
+// menu = {
+//   width: 400,
+//   height: 600,
+//   title: "My menu"
+// };
+// Please note that multiplyNumeric does not need to return anything. It should modify the object in-place.
+// P.S. Use typeof to check for a number here.
