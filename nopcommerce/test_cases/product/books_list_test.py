68675d9ced70a54f980ee1adee50dd64ea86e002
Nash-kenra <nash.kenra@gmail.com>
2024-01-10 11:53:44 -0600

    added sorting test to books

--- nopcommerce/test_cases/product/books_list_test.py
+++ nopcommerce/test_cases/product/books_list_test.py
@@ -1,11 +1,11 @@
 from page_objects.product.books_list_page import BooksListPage
 from test_cases.base_test import BaseTest
 import time
-
+import re
 
 class TestBooksListPage (BaseTest):
 
-    # Testcase for jewelryPage
+    # Testcase for booksPage
     def test_books_list_navigation(self, browser):
         list_page = BooksListPage(browser)       
         page_title= list_page.navigate_to_product_page()  
@@ -18,7 +18,33 @@ class TestBooksListPage (BaseTest):
         list_page.navigate_to_product_page()  
         cart_page_title = list_page.add_product_to_cart()  
         print (cart_page_title)
-        assert cart_page_title == "Shopping cart" 
+        assert cart_page_title == "Shopping cart"
+    
+    def test_books_sort_AtoZ(self, browser):
+        books_list_page = BooksListPage(browser)       
+        books_list_page.navigate_to_product_page()
+        sorted_product_names = books_list_page.sort_products_AtoZ()
+        assert sorted_product_names == sorted(sorted_product_names)
+
+    def test_books_sort_ZtoA(self, browser):
+        books_list_page = BooksListPage(browser)
+        books_list_page.navigate_to_product_page()
+        sorted_product_names = books_list_page.sort_products_ZtoA()
+        assert sorted_product_names == sorted(sorted_product_names, reverse=True)
+
+    def test_products_sort_price_lowtohigh(self, browser):
+        books_list_page = BooksListPage(browser)
+        books_list_page.navigate_to_product_page()
+        sorted_product_prices = books_list_page.sort_products_price_lowtohigh()
+        prices_as_float = [float(re.sub(r'[^\d.]', '', price)) for price in sorted_product_prices]
+        assert prices_as_float == sorted(prices_as_float)
+
+    def test_products_sort_price_hightolow(self, browser):
+        books_list_page = BooksListPage(browser)
+        books_list_page.navigate_to_product_page()
+        sorted_product_prices = books_list_page.sort_products_price_hightolow()
+        prices_as_float = [float(re.sub(r'[^\d.]', '', price)) for price in sorted_product_prices]
+        assert prices_as_float == sorted(prices_as_float, reverse=True)
     
 
 
